e8b0352dc044ca871e88450e6b9c6781db88e605
Rain <rainelemental@gmail.com>
2013-01-21 14:37:29 -0500

    added exception

---
+++ library/Rain/DB/Exception/FetchModeException.php
@@ -0,0 +1,13 @@
+<?php
+
+// set the namespace
+namespace Rain\DB\Exception;
+
+/**
+ * StatementIterator class iterates the result of the query to save memory
+ */
+class FetchModeException extends \Exception
+{
+
+
+}
